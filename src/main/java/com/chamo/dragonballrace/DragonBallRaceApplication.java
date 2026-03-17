f16da7c494e150767ee2bf66465dea89374ae3bb
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-03-16 20:08:48 -0500

    First Commit

---
+++ src/main/java/com/chamo/dragonballrace/DragonBallRaceApplication.java
@@ -0,0 +1,13 @@
+package com.chamo.dragonballrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonBallRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonBallRaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chamo/dragonballrace/DragonBallRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamo.dragonballrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonBallRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
